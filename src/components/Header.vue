1ebbfbd6c225e2d42540a9b54bd2eb6b2e8f681d
dwinrynt <dwinuryantosukatanai@gmail.com>
2022-08-29 08:50:37 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <div class="navbar">
+            <h2>ABSENSI</h2>
+            <h2>SMKS TARUNA BHAKTI DEPOK</h2>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default{
+        name: 'Header'
+    }
+</script>
+
+<style>
+    .navbar{
+        width: 100vw;
+        background-color: #3bae9c;
+        display: block;
+        text-align: center;
+    }
+
+    h2{
+        color: white;
+    }
+</style>
