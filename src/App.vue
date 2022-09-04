0de10033394bf5b7fa2defd0481057bb0807a23f
dwinrynt <dwinuryantosukatanai@gmail.com>
2022-09-04 21:47:23 +0700

    gif

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,119 @@
 <template>
   <header>
     <Header />
+  </header>
+  <Hai />
+  <div class="container1 p-0 mt-5" v-if="kode_respon == 1">
     <div class="isi">
       <div class="foto">
         <img src="" alt="image">
       </div>
     </div>
-  </header>
-  <div class="container mt-5" style="min-width: 70vw;" v-if="kode_respon == 1">
-    <table class="table table-bordered border-secondary text-center" v-if="agendas.length > 0">
-      <thead>
-        <tr class="text-white" style="background-color: #3bae9c">
-          <th scope="col" colspan="2" rowspan="2">HARI/ KELAS</th>
-          <th scope="col" colspan="3" style="text-transform: capitalize;">{{ hari }}</th>
-        </tr>
-        <tr class="text-white" style="background-color: #3bae9c">
-          <th colspan="3">{{ agendas[0].nama_kelas }}</th>
-        </tr>
-        <tr style="background-color: #7de2d1">
-          <th scope="row">JAM-KE</th>
-          <th>WAKTU</th>
-          <th>MAPEL</th>
-          <th>GURU</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="no in agendas.length">
-          <!-- {{ agendas[no - 1].jam_awal }} -->
-          <td scope="row">{{ no }}</td>
-          <td>{{ agendas[no - 1].jam_awal.split(':')[0] }}:{{ agendas[no - 1].jam_awal.split(':')[1] }}-{{ agendas[no - 1].jam_akhir.split(':')[0] }}:{{ agendas[no - 1].jam_akhir.split(':')[1] }}</td>
-          <td>{{ agendas[no - 1].mapel }}</td>
-          <td style="text-transform: capitalize;">{{ agendas[no - 1].guru }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="isi2">
+      <div class="data">
+        <p class="nama">Dwi Nuryanto</p>
+        <p class="npsn">20229232</p>
+        <p class="kelas">XII RPL 2</p>
+        <p class="jurusan">RPL</p>
+      </div>
+    </div>
+    <!-- table guru -->
+    <div class="container mt-5" style="min-width: 70vw;">
+      <table class="table table-bordered border-secondary text-center">
+        <thead>
+          <tr class="text-white" style="background-color: #3bae9c">
+            <th scope="col" colspan="2" rowspan="2">HARI/ KELAS</th>
+            <th scope="col" colspan="3" rowspan="2" style="text-transform: capitalize;">Sabtu</th>
+          </tr>
+          <tr class="text-white" style="background-color: #3bae9c">
+          </tr>
+          <tr style="background-color: #7de2d1">
+            <th scope="row">JAM-KE</th>
+            <th>WAKTU</th>
+            <th>MAPEL</th>
+            <th>Kelas</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td scope="row">1</td>
+            <td>07.00-12.30</td>
+            <td>PBO</td>
+            <td style="text-transform: capitalize;">XII RPL 2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- table siswa -->
+    <div class="container mt-5" style="min-width: 70vw;" v-if="kode_respon == 1">
+      <table class="table table-bordered border-secondary text-center" v-if="agendas.length > 0">
+        <thead>
+          <tr class="text-white" style="background-color: #3bae9c">
+            <th scope="col" colspan="2" rowspan="2">HARI/ KELAS</th>
+            <th scope="col" colspan="3" style="text-transform: capitalize;">{{ hari }}</th>
+          </tr>
+          <tr class="text-white" style="background-color: #3bae9c">
+            <th colspan="3">{{ agendas[0].nama_kelas }}XII RPL 2</th>
+          </tr>
+          <tr style="background-color: #7de2d1">
+            <th scope="row">JAM-KE</th>
+            <th>WAKTU</th>
+            <th>MAPEL</th>
+            <th>GURU</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="no in agendas.length">
+            <!-- {{ agendas[no - 1].jam_awal }} -->
+            <td scope="row">{{ no }}</td>
+            <td>{{ agendas[no - 1].jam_awal.split(':')[0] }}:{{ agendas[no - 1].jam_awal.split(':')[1] }}-{{ agendas[no - 1].jam_akhir.split(':')[0] }}:{{ agendas[no - 1].jam_akhir.split(':')[1] }}</td>
+            <td>{{ agendas[no - 1].mapel }}</td>
+            <td style="text-transform: capitalize;">{{ agendas[no - 1].guru }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
+  
+  <div class="container2 p-0 mt-5" v-if="kode_respon == 2">
+    <div class="isi">
+      <div class="foto">
+        <img src="" alt="image">
+      </div>
+    </div>
+    <div class="isi2">
+      <div class="data">
+        <p class="nama">Dwi Nuryanto</p>
+        <p class="nip">20229232</p>
+      </div>
+    </div>
+    <div class="container" style="min-width: 70vw; display: flex; justify-content: center; margin-top: 100px;">
+      <div style="width:30%; height:0; padding-bottom:30%; position:relative;">
+        <Hai />
+        <h3 class="text-center">Hati-hati di jalan!</h3>
+      </div>
+    </div>
+  </div>
+
+  <div class="container2 p-0 mt-5" v-if="kode_respon == 3">
+    <div class="isi">
+      <div class="foto">
+        <img src="" alt="image">
+      </div>
+    </div>
+    <div class="isi2">
+      <div class="data">
+        <p class="nama">Dwi Nuryanto</p>
+        <p class="npsn">20229232</p>
+        <p class="kelas">XII RPL 2</p>
+        <p class="jurusan">RPL</p>
+      </div>
+    </div>
+    <div class="container" style="min-width: 70vw; display: flex; justify-content: center; margin-top: 100px;">
+      <video src=""></video>
+    </div>
+  </div>
+  
   <input type="text" name="" class="input-rfid" autofocus>
 </template>
 
@@ -53,7 +132,27 @@ header {
 .isi {
   display: flex;
   justify-content: center;
-  margin-top: 100px;
+  margin-top: auto 100px;
+}
+
+.isi2 {
+  display: flex;
+  justify-content: center;
+}
+
+.data {
+  text-align: center;
+  line-height: 30px;
+  margin-top: 30px;
+}
+
+.nama {
+  font-size: 50px;
+  font-weight: 500;
+}
+
+.npsn, .kelas, .jurusan {
+  font-size: 30px;
 }
 
 .foto {
@@ -85,6 +184,8 @@ import HelloWorld from './components/HelloWorld.vue';
 import Header from './components/Header.vue';
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
+import Hai from './components/Hai.vue';
+import Hai1 from './components/Hai.vue';
 
 const kode_respon = ref()
 const message_kode = ref()
---
+++ src/components/Hai.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <video>
+            <source src="@/assets/hai.mp4" type="video.mp4">
+            <source src="@/assets/hai.ogg" type="video.ogg">
+        </video>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Hai'
+    }
+</script>
